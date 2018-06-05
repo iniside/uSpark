79a085aa8fbc7125fb44e48700fa721fec0630be
Lukasz 'iniside' Baran <xeavien@gmail.com>
2018-06-05 19:54:52 +0200

    Add project files.

---
+++ uSpark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uSpark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
